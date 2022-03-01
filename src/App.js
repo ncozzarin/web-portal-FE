f72aa023742c808239c3fee85b2931375981c1a3
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2022-03-01 18:28:19 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Hey there!
+        </div>
+
+        <div className="bio">
+        I am Nick and I worked on the IT field as a QA and a Front End developer... How are you doing today? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
